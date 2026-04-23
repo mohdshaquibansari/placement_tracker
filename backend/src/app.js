40ab7ff0a8c9c1f17e8a830d9c941b7b33cf6b6c
Aaquib <118913864+mohdshaquibansari@users.noreply.github.com>
2026-04-24 00:34:23 +0530

    fix cors final

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,13 @@ import routes from './routes/index.js';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://placement-tracker-tau.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '2mb' }));
